e167f17518d1898f7b91823e025f8b8c73009fe4
klamaj <kri.lamaj@gmail.com>
2022-09-03 12:39:00 +0300

    Db Using SQL Lite

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DB Connection
+builder.Services.AddDbConnection(builder.Configuration);
 
 var app = builder.Build();
 
