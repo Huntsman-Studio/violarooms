ea52498b5c93f102308c32abd87b7c0c5450b24e
klamaj <kri.lamaj@gmail.com>
2022-09-03 13:12:47 +0300

    Remove Vue.js

--- API/Controllers/Customers.cs
+++ API/Controllers/Customers.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,10 +6,23 @@ namespace API.Controllers;
 
 public class Customers : BaseApiController
 {
+    private readonly IGenericRepository<Customer> _customerRepo;
+    public Customers(IGenericRepository<Customer> customerRepo)
+    {
+        _customerRepo = customerRepo;
+    }
+
     // GetCustomers
     [HttpGet("get")]
     public async Task<ActionResult<Customer>> GetCustomers()
     {
-        return Ok();
+        return Ok(await _customerRepo.ListAllAsync());
+    }
+
+    // GetCustomerById
+    [HttpGet("get/{id}")]
+    public async Task<ActionResult<Customer>> GetCustomerById(int id)
+    {
+        return Ok(await _customerRepo.GetByIdAsync(id));
     }
 }
