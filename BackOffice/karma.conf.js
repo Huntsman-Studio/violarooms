4b618396acf1678514eb9484585e3e3be7cd49e5
klamaj <kri.lamaj@gmail.com>
2022-09-03 13:22:17 +0300

    code rf

--- BackOffice/karma.conf.js
+++ BackOffice/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Dashboard'),
+      dir: require('path').join(__dirname, './coverage/BackOffice'),
       subdir: '.',
       reporters: [
         { type: 'html' },
